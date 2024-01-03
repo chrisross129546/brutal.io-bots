851cfcd749f37050091775625269be6f55eee9d4
chrisross129546 <141733376+chrisross129546@users.noreply.github.com>
2024-01-03 22:33:56 +0000

    Update protocol.js

--- protocol.js
+++ protocol.js
@@ -2,7 +2,7 @@ export const generateInitialiserPacket = () => {
     const buffer = new ArrayBuffer(5);
     const view = new DataView(buffer);
     
-    view.setUint8(0, 1);
+    view.setUint8(0, 191);
     view.setUint16(1, 960 / 10, true);
     view.setUint16(3, 540 / 10, true);
     
